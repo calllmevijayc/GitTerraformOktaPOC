c768093763bc9ca2a04bc00b6870b0281e91a6dc
vijay <callmevijayc@gmail.com>
2021-09-21 14:16:25 +0530

    main.tf file update

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "self-learning2405"
+#The configuration for the `remote` backend.
+     terraform {
+       backend "remote" {
+          #The name of your Terraform Cloud organization.
+		 hostname = "app.terraform.io"
+         organization = "self-learning2405"
+ 
+          #The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "GitTerraformOktaPOC"
+         }
+       }
+     }
 
-    workspaces {
-      name = "GitTerraformOktaPOC"
-    }
-  }
-}
+      #in example resource that does nothing.
+     resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing!"
+       }
+     }

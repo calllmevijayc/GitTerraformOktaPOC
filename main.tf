8695a641c34cb56fae0b41dfdf8d4ea225fb62c6
vijay <callmevijayc@gmail.com>
2021-09-21 12:49:29 +0530

    main.tf file update

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@
      terraform {
        backend "remote" {
          # The name of your Terraform Cloud organization.
-         organization = ""self-learning2405
+          hostname = "app.terraform.io"
+	  organization = ""self-learning2405
  
           # The name of the Terraform Cloud workspace to store Terraform state files in.
          workspaces {
